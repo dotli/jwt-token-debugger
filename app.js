2faed47dea9f72731ea9b652eeb800bd0d403326
lixin <lixin@ihealthinkcare.com>
2020-12-10 14:20:29 +0800

    feat: add datetime parse and description

--- app.js
+++ app.js
@@ -27,6 +27,15 @@ new Vue({
     },
     methods: {
         set: function (header, playload, signature) {
+            const dateReplacer = function (name, value, desc) {
+                const d = new Date(Number.parseInt(value) * 1000).toString()
+                return `<span title="${desc}">${name}</span>: <span title="${d}">${value}</span>`
+            }
+            playload = playload
+                .replace(/("nbf"): (\d+)/, function (_, $1, $2) { return dateReplacer($1, $2, 'Not valid before (seconds since Unix epoch)') })
+                .replace(/("iat"): (\d+)/, function (_, $1, $2) { return dateReplacer($1, $2, 'Issued at (seconds since Unix epoch)') })
+                .replace(/("exp"): (\d+)/, function (_, $1, $2) { return dateReplacer($1, $2, 'Expiration time (seconds since Unix epoch)') })
+                .replace(/("auth_time"): (\d+)/, function (_, $1, $2) { return dateReplacer($1, $2, 'Time when authentication occurred') })
             this.output.header = header;
             this.output.playload = playload;
             this.output.signature = signature;
